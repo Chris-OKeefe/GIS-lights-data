6d137afdf1258be685085f140b268acdf04fa5c1
Chris O'Keefe <chris.okeefe.79@gmail.com>
2016-09-23 12:44:38 -0700

    New borders data

--- GIS-lights.R
+++ GIS-lights.R
@@ -15,7 +15,7 @@ lights <- raster("./build/input/lights-data-2013/F182013.v4c_web.stable_lights.a
 crs_lights = crs(lights)
 
 #US Borders data
-borders <- readOGR(dsn = path.expand("~/Documents/Programs/R-general/GIS-lights-data/build/input/us-national-borders"), layer = "cb_2015_us_nation_5m")
+borders <- readOGR(dsn = path.expand("~/Documents/Programs/R-general/GIS-lights-data/build/input/us-national-borders"), layer = "USA_adm0")
 borders_wgs84 <- spTransform(borders, crs_lights) #convert borders to wgs84 (from NAD83)
 
 #Testing-plot lights data, US borders
