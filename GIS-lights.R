b3f84ae1f17e93670b4ec71e8a4fed33628cebef
Chris O'Keefe <chris.okeefe.79@gmail.com>
2016-09-23 12:48:54 -0700

    README edits

--- GIS-lights.R
+++ GIS-lights.R
@@ -12,14 +12,12 @@ rm(list = ls())
 #Read in Data
 #Lights raster data
 lights <- raster("./build/input/lights-data-2013/F182013.v4c_web.stable_lights.avg_vis.tif") #cleaned up version of avg_vis files
-crs_lights = crs(lights)
 
 #US Borders data
 borders <- readOGR(dsn = path.expand("~/Documents/Programs/R-general/GIS-lights-data/build/input/us-national-borders"), layer = "USA_adm0")
-borders_wgs84 <- spTransform(borders, crs_lights) #convert borders to wgs84 (from NAD83)
 
 #Testing-plot lights data, US borders
 plot(lights)
-plot(borders_wgs84, add = T) #they don't align, but resolutions seem to map ok.
+plot(borders, add = T) #they don't align, but resolutions seem to map ok.
 
 #NPS shapefiles
--- README.md
+++ README.md
@@ -4,3 +4,11 @@ GIS-based indicator of light pollution levels within National Parks.
 [Lights data](https://ngdc.noaa.gov/eog/dmsp/downloadV4composites.html)
 
 Some [notes](http://economics.mit.edu/files/8945) on using lights data
+
+To do:
+* Fix basic plotting issue
+* Overlay US National Parks Data
+* Extract data to generate park-level indicator
+	* Consider removing 1 mile boundary around park border
+* Find international national parks data for non-US sites
+* Repeat for US Congressional districts, US counties (with population data?), US municipalities
