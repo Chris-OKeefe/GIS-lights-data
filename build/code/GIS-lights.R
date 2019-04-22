0c9d3fe1728fc50ac263f2ad19efe3c559a503ad
Chris O'Keefe <chris.okeefe.79@gmail.com>
2019-04-22 13:23:46 -0400

    Added NPS shapefiles

--- build/code/GIS-lights.R
+++ build/code/GIS-lights.R
@@ -20,16 +20,23 @@ us_borders <- getData('GADM', country = "USA", level = 0)
 #borders <- readOGR(dsn = path.expand("~/Documents/Programs/R-general/GIS-lights-data/build/input/us-national-borders"), layer = "USA_adm0")
 
 #Plot lights data, US borders
-pdf("lights-test.pdf")#not sure if it's worth investing in making map look good.
-        plot(lights, xaxt = 'n', yaxt = 'n')
-        plot(us_borders, add = T, lwd = .25) 
-dev.off()
+#pdf("lights-test.pdf")#not sure if it's worth investing in making map look good.
+#        plot(lights, xaxt = 'n', yaxt = 'n')
+#        plot(us_borders, add = T, lwd = .25) 
+#dev.off()
 
-#Cropping?
-pdf("lights-test-us.pdf")
-    test.sub <- crop(lights, extent(us_borders)) #may not work, gives an error & IDK how to check results
-    test.sub <- mask(test.sub, us_borders) #makes sure GIS features from raster properly associated with new object. Seems to take a long time
+#Cropping
+lights_crop <- crop(lights, extent(us_borders)) #may not work, gives an error & IDK how to check results
+lights_crop <- mask(lights_crop, us_borders) #makes sure GIS features from raster properly associated with new object. Seems to take a long time
+
 #then re-do plot like in above
-dev.off()
+#pdf("lights-test-us.pdf")
+    plot(lights_crop, xaxt = 'n', yaxt = 'n')
+    #plot(us_borders, add = T, lwd = .25) 
+#dev.off()
 
 #NPS shapefiles
+nps <- readOGR(dsn = path.expand("~/Documents/Programs/R-general/GIS-lights-data/build/input/nps-boundaries"), layer = "nps_boundary")
+nps_84 <- spTransform(nps, crs(us_borders))
+
+#pdf("lights-test-nps.pdf")
